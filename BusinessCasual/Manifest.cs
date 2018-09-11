2d12e7a4330f228cbcfff763c417769be60633c7
psijkof <koffemans@hotmail.com>
2018-09-11 18:39:42 +0100

    added items, very much WIP

---
+++ BusinessCasual/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "BusinessCasual",
+    Author = "The Orchard Team",
+    Website = "https://orchardproject.net",
+    Version = "2.0.0",
+    Description = "BusinessCasual"
+)]
